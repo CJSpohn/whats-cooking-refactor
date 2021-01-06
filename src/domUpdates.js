92498c6ab181f69d136777d8d927f5daa1f9b5d1
conconartist <conconart.info@gmail.com>
2021-01-06 15:58:13 -0700

    Add methods to domUpdates for page onload

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,6 +1,41 @@
 let domUpdates = {
-    updateDomMethod1() {
-        console.log("hellllo")
+    greetUser(user) {
+        const userName = document.querySelector('.user-name');
+        userName.innerHTML =
+        user.name.split(' ')[0] + ' ' + user.name.split(' ')[1][0];
+    },
+    getFavorites(user) {
+        if (user.favoriteRecipes.length) {
+          user.favoriteRecipes.forEach(recipe => {
+            document.querySelector(`.favorite${recipe.id}`).classList.add('favorite-active')
+          })
+        } 
+    },
+    populateCards(cardArea, recipes, user) {
+        cardArea.innerHTML = '';
+        if (cardArea.classList.contains('all')) {
+          cardArea.classList.remove('all')
+        }
+        recipes.forEach(recipe => {
+          cardArea.insertAdjacentHTML('afterbegin', `
+          <div id='${recipe.id}' class='card'>
+            <header id='${recipe.id}' class='card-header'>
+              <label for='add-button' class='hidden'>Click to add recipe</label>
+              <button id='${recipe.id}' aria-label='add-button' class='add-button card-button'>
+              <img id='${recipe.id} favorite' class='add'
+                src='https://image.flaticon.com/icons/svg/32/32339.svg' alt='Add to
+                recipes to cook'>
+              </button>
+              <label for='favorite-button' class='hidden'>Click to favorite recipe
+              </label>
+              <button id='${recipe.id}' aria-label='favorite-button' class='favorite favorite${recipe.id} card-button'></button>
+            </header>
+            <span id='${recipe.id}' class='recipe-name'>${recipe.name}</span>
+            <img id='${recipe.id}' tabindex='0' class='card-picture'
+              src='${recipe.image}' alt='click to view recipe for ${recipe.name}'>
+          </div>`)
+        })
+        this.getFavorites(user);
     }
 }
 
--- src/scripts.js
+++ src/scripts.js
@@ -36,16 +36,14 @@ const getData = () => {
       cookbook = new Cookbook(dataset[1]);
       ingredients = dataset[2];
       //DOM UPDATES
-      greetUser();
-      populateCards(cookbook.recipes);
+      domUpdates.greetUser(user);
+      domUpdates.populateCards(cardArea, cookbook.recipes, user);
       //DOM UPDATES
     });
 }
 
 const onStartup = () => {
   getData();
-  console.log(domUpdates.updateDomMethod1());
-  domUpdates.updateDomMethod1();
 }
 
 const viewFavorites = () => {
@@ -78,12 +76,6 @@ const viewFavorites = () => {
       </div>`)
     })
   }
-}
-
-const greetUser = () => {
-  const userName = document.querySelector('.user-name');
-  userName.innerHTML =
-  user.name.split(' ')[0] + ' ' + user.name.split(' ')[1][0];
 }
 
 const favoriteCard = (event) => {
@@ -124,63 +116,31 @@ const displayDirections = (event) => {
   let cost = recipeObject.calculateCost()
   let costInDollars = (cost / 100).toFixed(2)
   cardArea.classList.add('all');
-  cardArea.innerHTML = `<h3>${recipeObject.name}</h3>
-  <p class='all-recipe-info'>
-  <strong>It will cost: </strong><span class='cost recipe-info'>
-  $${costInDollars}</span><br><br>
-  <strong>You will need: </strong><span class='ingredients recipe-info'></span>
-  <strong>Instructions: </strong><ol><span class='instructions recipe-info'>
-  </span></ol>
-  </p>`;
+  cardArea.innerHTML = `
+  <h3>${recipeObject.name}</h3>
+    <p class='all-recipe-info'>
+    <strong>It will cost: </strong><span class='cost recipe-info'>
+    $${costInDollars}</span><br><br>
+    <strong>You will need: </strong><span class='ingredients recipe-info'></span>
+    <strong>Instructions: </strong><ol><span class='instructions recipe-info'>
+    </span></ol>
+    </p>`;
   let ingredientsSpan = document.querySelector('.ingredients');
   let instructionsSpan = document.querySelector('.instructions');
   recipeObject.ingredients.forEach(ingredient => {
-    ingredientsSpan.insertAdjacentHTML('afterbegin', `<ul><li>
-    ${ingredient.quantity.amount.toFixed(2)} ${ingredient.quantity.unit}
-    ${ingredient.name}</li></ul>
+    ingredientsSpan.insertAdjacentHTML('afterbegin', `
+    <ul>
+      <li>${ingredient.quantity.amount.toFixed(2)} ${ingredient.quantity.unit}
+      ${ingredient.name}</li>
+    </ul>
     `)
   })
   recipeObject.instructions.forEach(instruction => {
-    instructionsSpan.insertAdjacentHTML('beforebegin', `<li>
-    ${instruction.instruction}</li>
+    instructionsSpan.insertAdjacentHTML('beforebegin', `
+    <li>${instruction.instruction}</li>
     `)
   })
 }
-
-const getFavorites = () => {
-  if (user.favoriteRecipes.length) {
-    user.favoriteRecipes.forEach(recipe => {
-      document.querySelector(`.favorite${recipe.id}`).classList.add('favorite-active')
-    })
-  } else return
-}
-
-const populateCards = (recipes) => {
-  cardArea.innerHTML = '';
-  if (cardArea.classList.contains('all')) {
-    cardArea.classList.remove('all')
-  }
-  recipes.forEach(recipe => {
-    cardArea.insertAdjacentHTML('afterbegin', `<div id='${recipe.id}'
-    class='card'>
-        <header id='${recipe.id}' class='card-header'>
-          <label for='add-button' class='hidden'>Click to add recipe</label>
-          <button id='${recipe.id}' aria-label='add-button' class='add-button card-button'>
-            <img id='${recipe.id} favorite' class='add'
-            src='https://image.flaticon.com/icons/svg/32/32339.svg' alt='Add to
-            recipes to cook'>
-          </button>
-          <label for='favorite-button' class='hidden'>Click to favorite recipe
-          </label>
-          <button id='${recipe.id}' aria-label='favorite-button' class='favorite favorite${recipe.id} card-button'></button>
-        </header>
-          <span id='${recipe.id}' class='recipe-name'>${recipe.name}</span>
-          <img id='${recipe.id}' tabindex='0' class='card-picture'
-          src='${recipe.image}' alt='click to view recipe for ${recipe.name}'>
-    </div>`)
-  })
-  getFavorites();
-};
 
 window.onload = onStartup();
 homeButton.addEventListener('click', cardButtonConditionals);
