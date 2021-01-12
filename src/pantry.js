ba5ae19f4e4c490a345a65cc2f53f560f374816e
Christina Cook <cmc.cook7@gmail.com>
2021-01-12 13:34:11 -0700

    Build checkPantryForIngredients method in pantry class

--- src/pantry.js
+++ src/pantry.js
@@ -2,6 +2,26 @@ class Pantry {
   constructor(userIngredients) {
     this.contents = userIngredients;
   }
+
+  checkPantryForIngredients(recipe) {
+    let ingredientsNeeded = [];
+    recipe.ingredients.forEach(recipeIngredient => {
+      this.contents.forEach(pantryIngredient => {
+        if (pantryIngredient.ingredient === recipeIngredient.id) {
+          if (pantryIngredient.amount < recipeIngredient.quantity.amount) {
+            let amountNeeded = recipeIngredient.quantity.amount - pantryIngredient.amount;
+            let ingredientName = recipe.ingredientsData.find(ingredient => ingredient.id === pantryIngredient.ingredient).name;
+            ingredientsNeeded.push({
+              ingredient: ingredientName,
+              amount: amountNeeded,
+              unit: recipeIngredient.quantity.unit})
+          }
+        }
+      })
+      return ingredientsNeeded;
+    })
+
+  }
 }
 
 export default Pantry;
