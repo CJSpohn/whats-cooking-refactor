edeae84bf89912e7ade53b98fbec6f93f470985b
conconartist <conconart.info@gmail.com>
2021-01-13 11:46:00 -0700

    Add test blocks to Pantry test file

--- test/pantry-test.js
+++ test/pantry-test.js
@@ -4,25 +4,37 @@ import Pantry from '../src/pantry.js';
 import Recipe from '../src/recipe.js';
 import recipeData from '../src/data/data/recipes.js';
 import ingredientsData from '../src/data/data/ingredients.js';
+import users from '../src/data/data/users.js';
 
-let pantry;
+let pantry, recipe;
 
 describe('Pantry', () => {
     beforeEach(() => {
-        pantry = new Pantry(userIngredients); 
+        pantry = new Pantry(users[0].pantry); 
+        recipe = new Recipe(recipeData[4], ingredientsData);
     });
-    
-    it('should determine whether there are enough ingredients in the pantry to cook a given meal', () => {
-
-    });    
+      
     it('should determine the amount of ingredients still needed to cook a given meal, based on what is in the user\'s pantry', () => {
-
+        expect(pantry.checkPantryForIngredients(recipe)).to.eql([ { ingredient: 'egg albumen', amount: 2, unit: 'large' } ]);
     });
     it('should remove the ingredients used for a given meal from the user\'s pantry, once the meal has been cooked', () => {
-
-    });
-    it('should only remove ingredients if the user has a list of meals to cook', () => {
-
+        expect(pantry.findItemsToRemove(recipe)).to.eql([
+            { id: 2048, amount: 2 },
+            { id: 18371, amount: 0.33 },
+            { id: 20090, amount: 1.13 },
+            { id: 93784, amount: 2 },
+            { id: 4582, amount: 0.25 },
+            { id: 1124, amount: 3 },
+            { id: 93625, amount: 2 },
+            { id: 12220, amount: 2 },
+            { id: 10118375, amount: 2 },
+            { id: 19304, amount: 2 },
+            { id: 11413, amount: 0.75 },
+            { id: 2047, amount: 0.75 },
+            { id: 93696, amount: 0.75 },
+            { id: 93760, amount: 0.25 },
+            { id: 14412, amount: 1 },
+            { id: 93626, amount: 2 }
+          ]);
     });
 })
-// access user data to find pantry property
